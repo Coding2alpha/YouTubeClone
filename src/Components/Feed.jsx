cf3d0523801f8be264eb233cb4be977e481b2664
Coding2alpha <omsinghthakur2502@gmail.com>
2024-01-25 02:03:14 +0530

    fetch-api

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -2,8 +2,16 @@ import { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
 import { categories } from "../utils/constants";
 import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/FetchFromApi";
 
 const Feed = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("new");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=&${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack
       sx={{
@@ -17,7 +25,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography>Copyright 2024 </Typography>
       </Box>
       <Box p={2} sx={{ overflow: "auto", height: "90vh" }}>
@@ -29,7 +40,7 @@ const Feed = () => {
         >
           New <span style={{ color: "#F31503" }}>Videos</span>
         </Typography>
-        <Videos />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
