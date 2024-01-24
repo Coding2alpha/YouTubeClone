f5e4981836e646192a5de4fd7a93229b69ca28a1
Coding2alpha <omsinghthakur2502@gmail.com>
2024-01-24 16:26:54 +0530

    2

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,4 +1,26 @@
+import { useState, useEffect } from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import { categories } from "../utils/constants";
+import Sidebar from "./Sidebar";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: { sx: "column", md: "row" },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography>Copyright</Typography>
+      </Box>
+    </Stack>
+  );
 };
 export default Feed;
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,5 @@
+import { Stack,Box } from "@mui/material";
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+export default Sidebar;
