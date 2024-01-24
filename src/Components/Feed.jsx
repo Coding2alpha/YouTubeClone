e8d730689fe3ac6bb6e29fe99a7973f34d7ae21b
Coding2alpha <omsinghthakur2502@gmail.com>
2024-01-24 19:48:07 +0530

    3

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
 import { categories } from "../utils/constants";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
@@ -18,7 +18,18 @@ const Feed = () => {
         }}
       >
         <Sidebar />
-        <Typography>Copyright</Typography>
+        <Typography>Copyright 2024 </Typography>
+      </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "90vh" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        <Videos />
       </Box>
     </Stack>
   );
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,37 @@
-import { Stack,Box } from "@mui/material";
+import { Stack, Box, colors } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const selected = "New";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflow: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <Box key={category.name}>
+          <button
+            className="category-btn"
+            style={{
+              background: category.name === selected && "#FC1503",
+              color: "white",
+            }}
+          >
+            <span
+              style={{ color: category.name === selected ? "white" : "red" }}
+            >
+              {category.icon}
+            </span>
+            <span>{category.name}</span>
+          </button>
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 export default Sidebar;
